305a9e0918f39eca60957c017dc1c91d5b5119f7
Raúl Alejandro <RaulAlejandro_RodriguezR@hotmail.com>
2024-07-26 15:17:34 -0600

    Update observable.py

--- Control_GPS/programa_control/observable.py
+++ Control_GPS/programa_control/observable.py
@@ -1,17 +1,17 @@
 class ObservableBool:
-    def __init__(self, initial_value=False):
-        self._value = initial_value
-        self._prev_value = initial_value
+    def __init__(self, valor_inicial=False):
+        self.valor = valor_inicial
+        self.valor_prev = valor_inicial
 
     @property
     def value(self):
-        return self._value
+        return self.valor
 
     @value.setter
-    def value(self, new_value):
-        if new_value != self._value:
-            self._prev_value = self._value
-            self._value = new_value
+    def value(self, nuevo_valor):
+        if new_value != self.valor:
+            self.valor_prev = self.valor
+            self.valor = nuevo_valor
 
     def has_changed(self):
-        return self._value != self._prev_value
+        return self.valor != self.valor_prev
